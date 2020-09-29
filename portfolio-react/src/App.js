ce8630d8f826b68961eb8f7bfa91a918220e2732
Mac Parthum <macparthum@gmail.com>
2020-09-29 19:42:50 -0400

    home about and contact page routes all working

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -6,6 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
   constructor(props) {
@@ -35,20 +38,28 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
+
           <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Mac Parthum</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
             <Nav className="ml-auto">
+
                 <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/">About</Link>
-                <Link className="nav-link" to="/">Contact</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+
               </Nav>
             </Navbar.Collapse>
-          </Navbar>
 
-          <Footer>
-          </Footer>
+          </Navbar>
+    
+            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subtitle}  text={this.state.home.text} />} />
+            <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+            <Route path="/contact" render={() => <ContactPage title={this.state.about.title} />} />
+
+          <Footer />
 
         
 
