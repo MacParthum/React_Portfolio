c6066b23eb051d8950db9b5a06c39fe2719b5907
Mac Parthum <macparthum@gmail.com>
2020-09-29 16:50:34 -0400

    yarn add react-router-dom

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "./App.css";
+import {BrowserRouter as Router, Route, Link }
+ from 'react-router-dom';
+ import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
