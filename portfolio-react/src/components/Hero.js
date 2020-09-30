c9df5e947b8e9c9e6863c8969d96aa66bd03e709
Mac Parthum <macparthum@gmail.com>
2020-09-29 23:50:57 -0400

    working on carousel

--- portfolio-react/src/components/Hero.js
+++ portfolio-react/src/components/Hero.js
@@ -7,13 +7,14 @@ import Col from "react-bootstrap/Col";
 function Hero(props) {
   return (
 
-    <Jumbotron>
-      <Container>
-        <Row>
-          <Col>
-            {props.title && <h1>{props.title}</h1>}
+    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+  
+      <Container fluid={true}>
+        <Row className="justify-content-center py-5">
+          <Col md={8} sm={6}>
+            {props.title && <h1 className="display-3 font-weight-bolder">{props.title}</h1>}
             {props.subTitle && <h3>{props.subTitle}</h3>}
-            {props.text && <h3>{props.text}</h3>}
+            {props.text && <h3 className="Lead font-weight-light">{props.text}</h3>}
           </Col>
         </Row>
       </Container>
